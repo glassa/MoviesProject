ae4307cb757daab18ea02c8eae1cb0fd9cbd85fa
imdokdokhe <tonykslee1@gmail.com>
2016-10-19 14:24:10 -0700

    Created Tab Fragments for the main 5 buttons

---
+++ app/src/main/java/glassa/tacoma/uw/edu/moviesproject/Tab3FindMovies.java
@@ -0,0 +1,29 @@
+package glassa.tacoma.uw.edu.moviesproject;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Tab3FindMovies extends Fragment {
+
+
+    public Tab3FindMovies() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab3_find_movies, container, false);
+    }
+
+}
