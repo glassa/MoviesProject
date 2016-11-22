db630b0187552166f200c23abd9a3791b2e07b62
imdokdokhe <tonykslee1@gmail.com>
2016-11-21 18:06:10 -0800

    resized feed

--- app/src/main/java/glassa/tacoma/uw/edu/moviesproject/Tab3FindMovies.java
+++ app/src/main/java/glassa/tacoma/uw/edu/moviesproject/Tab3FindMovies.java
@@ -4,7 +4,6 @@ package glassa.tacoma.uw.edu.moviesproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,14 +22,15 @@ public class Tab3FindMovies extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         Intent intent = new Intent(getActivity(), SearchMovieActivity.class);
-        startActivity(intent);
+//        startActivity(intent);
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tab3_find_movies, container, false);
     }
-
 }
